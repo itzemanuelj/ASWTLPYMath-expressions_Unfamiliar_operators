5a921f97b809c2d3dcc42e9728a8dd7a0cc72b6c
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-21 02:41:39 -0400

    inital commit math expressions unfamilar-operators

---
+++ Math expressions_Unfamiliar_operators.py
@@ -0,0 +1,37 @@
+# Type the modulo operator
+
+1 % 6 
+
+# What is the name of this operator (one word, lowercase)?
+
+# %
+
+# modulou
+
+# Write the code that finds the remainder when 8 is divided by 3.
+
+num = 8 % 3
+
+# Find the remainder when you divide 12 by 3. Assign the result to the variable left_over.
+
+# left_over = 12 % 3 
+# print(left_over)
+
+# total = total + 5
+
+total += 5
+
+# Code the shorthand version of the following statement:
+
+# loss = loss - 5
+
+loss -= 5
+
+# Find the remainder when you divide one number by another and assign the result to a variable. Make up the numbers and the variable name.
+
+number_one = 100 % 25
+remander_one = number_one
+print(remander_one)
+
+Use shorthand code to multiply a variable by a number and assign the result to the same variable. Make up the variable name and the number.
+
