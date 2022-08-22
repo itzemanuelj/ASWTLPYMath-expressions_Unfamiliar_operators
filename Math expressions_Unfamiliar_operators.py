79096338ad667a8abe262a9584f9623cb37dd2f0
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-22 00:29:35 -0400

    all awnsers submitted

--- Math expressions_Unfamiliar_operators.py
+++ Math expressions_Unfamiliar_operators.py
@@ -33,5 +33,35 @@ number_one = 100 % 25
 remander_one = number_one
 print(remander_one)
 
-Use shorthand code to multiply a variable by a number and assign the result to the same variable. Make up the variable name and the number.
+# Use shorthand code to multiply a variable by a number and assign the result to the same variable. Make up the variable name and the number.
 
+random_num = 56
+random_num *= 56
+
+# Find what's left over when you divide one variable by another and assign the result to a third variable. Make up the variable names.
+
+mod_num = 20
+mod_two = 5
+left_over = mod_num % mod_two
+
+# Using shorthand code, subtract one variable from another and assign the result to the first variable. Make up the variable names.
+
+total = 100
+cost = 25
+
+total -= cost 
+
+# In a single line of code, display the remainder—that is, what's left over—when 100 is divided by 11.
+
+remaider = 100 % 11
+
+# Rewrite lines 2 and 3 using the shorthand you used in this chapter.
+
+x = 100
+x = x + 3
+x = x / x
+print(x)
+
+
+x += 3
+print(x)
